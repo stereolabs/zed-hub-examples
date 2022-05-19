7ec12bb4a661cc126470dd8e670def5b3db917eb
antoine <antoine.lassagne@stereolabs.com>
2022-05-19 10:17:58 +0200

    reworked tutorial 4 and 5 - untested

--- tutorials/tutorial_05_remote_function_calls/python/main.py
+++ tutorials/tutorial_05_remote_function_calls/python/main.py
@@ -20,134 +20,13 @@
 
 import pyzed.sl_iot as sliot
 import time
-import paho.mqtt.client as mqttClient
 import os
 import json
 
-from typing import Callable
+# from typing import Callable
 
-
-class slMqttClient:
-    def __init__(self):
-        if os.path.exists('/usr/local/sl_iot/settings/env.json'):
-            f = open('/usr/local/sl_iot/settings/env.json')
-            variables = json.load(f)
-            for key in variables.keys():
-                if key in os.environ:
-                    print("Original : ", os.environ[key])
-                else:
-                    os.environ[key] = variables[key]
-                print(key, ", ", os.environ[key])
-
-        else:
-            print("No env file found in /usr/local/sl_iot/settings/env.json")
-
-        #######################################################################
-        #                                                                     #
-        #   MQTT configuration   #
-        #   The docs are at https://cloud.stereolabs.com/doc/mqtt-api/        #
-        #                                                                     #
-        #######################################################################
-        broker_address = os.environ.get("SL_MQTT_HOST")  # Broker address
-        mqtt_port = int(os.environ.get("SL_MQTT_PORT"))  # Broker port
-        mqtt_user = "application"  # Connection username
-        app_token = os.environ.get("SL_APPLICATION_TOKEN")  # Connection password
-
-        app_name = os.environ.get("SL_APPLICATION_NAME")  # Connection password
-        self.device_id = str(os.environ.get("SL_DEVICE_ID"))
-        logs_topic = "/v1/devices/" + self.device_id + "/logs"
-
-        # Topic where the cloud publishes parameter modifications
-        self.app_ID = str(os.environ.get("SL_APPLICATION_ID"))
-
-        self.subscriptions = {}
-        # Client that listens to the app parameter modifications
-        client_id = "tutorial_5_remote_function"
-
-        self.client = mqttClient.Client(client_id)  # create new instance
-
-        print("Connecting MQTT")
-        # the callback is handled directly with MQTT, unlike in C++
-        #############     Get the environment variables      ##################
-        # set username and password
-        self.client.username_pw_set(mqtt_user, password=app_token)
-
-        self.client.on_connect = self.on_connect  # attach function to callback
-        self.client.on_disconnect = self.on_disconnect  # attach function to callback
-        self.client.on_message = self.on_message  # attach function to callback
-        self.client.on_publish = self.on_publish  # attach function to callback
-
-        print("Connecting to broker")
-        # connect to broker
-        self.client.connect(broker_address, port=mqtt_port)
-
-        #################        start MQTT CLIENT thread            ##################
-        self.client.loop_start()
-        print("Connected to MQTT")
-
-    ####### alert MQTT #######
-    def on_connect(self, client, userdata, flags, rc):
-        if rc == 0:
-            print("MQTT service connected to broker. Subscribing...")
-        else:
-            print("MQTT service Connection failed")
-
-    def on_disconnect(self, client, userdata, rc=0):
-        print("MQTT service disconnected")
-
-    def on_publish(self, client, userdata, result):  # create function for callback
-        print("message published")
-        pass
-
-    def subscribe_callback(self, remote_name: str, callback: Callable, callback_type: sliot.CALLBACK_TYPE, parameter_type: sliot.PARAMETER_TYPE):
-        topic = ""
-        parameter_type_addition = ""
-
-        if parameter_type == sliot.PARAMETER_TYPE.APPLICATION:
-            parameter_type_addition = "/apps/" + self.app_ID
-
-        if callback_type == sliot.CALLBACK_TYPE.ON_PARAMETER_UPDATE:
-            topic = "/v1/devices/" + self.device_id + \
-                parameter_type_addition + "/twin/update"
-        elif callback_type == sliot.CALLBACK_TYPE.ON_REMOTE_CALL:
-            topic = "/v1/devices/" + self.device_id + \
-                parameter_type_addition + "/functions/in"
-
-        if topic != "":
-            self.client.subscribe(topic)
-            if topic not in self.subscriptions:
-                self.subscriptions[topic] = {}
-            self.subscriptions[topic][remote_name] = callback
-            print("Subcribed to topic ")
-
-    def on_message(self, client, inference_thread_manager, message):
-        """
-        Note that you must subscribe a topic to be able to receive messages
-        (and of course a message must be published on this topic)
-        """
-        if message.topic in self.subscriptions:
-            message_received = json.loads(str(message.payload.decode()))
-            # Check all subscriptions
-            for remote_name in self.subscriptions[message.topic].keys():
-                # If a subscription fits with the remote name we received
-                if ("parameters.requested." + remote_name in message_received) or ('name' in message_received and message_received['name'] == remote_name):
-                    # Call the stored callback
-                    print(message_received)
-                    callback = self.subscriptions[message.topic][remote_name]
-                    b = callback(message_received)
-
-                    # If it's a remote call, we need to respond.
-                    if message.topic.endswith("/functions/in"):
-                        response = {
-                            "name": message_received["name"],
-                            "call_id": message_received["id"],
-                            "status": 0,
-                            "result": b
-                        }
-                        self.client.publish(message.topic.replace("/functions/in", "/functions/out"), json.dumps(response))
-
-
-def addition_callback(params: dict):
+def addition_callback(topic, message):
+    params = json.loads(message)
     num1 = params["parameters"]["num1"]
     num2 = params["parameters"]["num2"]
 
@@ -166,7 +45,6 @@ def addition_callback(params: dict):
 
 
 def main():
-    mqtt = slMqttClient()
     status_iot = sliot.HubClient.connect("callback_app")
 
     if status_iot != sliot.STATUS_CODE.SUCCESS:
@@ -178,6 +56,11 @@ def main():
     # If you want to test this on your machine, you'd better switch all your subscriptions to PARAMETER_TYPE.DEVICE.
     mqtt.subscribe_callback("tuto05_add", addition_callback,
                             sliot.CALLBACK_TYPE.ON_REMOTE_CALL, sliot.PARAMETER_TYPE.DEVICE)
+
+    # Set your parameter callback
+    callback_params = sliot.CallbackParameters()
+    callback_params.set_remote_callback("tuto05_add", sliot.CALLBACK_TYPE.ON_REMOTE_CALL)
+    sliot.HubClient.register_function(addition_callback, callback_params)
 
     print("Waiting for remote function to be called.")
     # Main loop
