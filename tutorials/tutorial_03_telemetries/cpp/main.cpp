85ff434980e73e9974de8bdc46c766c46b6e94bb
alassagne <antoine.lassagne@stereolabs.com>
2023-01-17 17:25:01 +0100

    update tuto 3 cpp

--- tutorials/tutorial_03_telemetries/cpp/main.cpp
+++ tutorials/tutorial_03_telemetries/cpp/main.cpp
@@ -31,40 +31,45 @@ using namespace sl;
 using namespace sl_iot;
 using json = sl_iot::json;
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // initialize the communication to zed hub, with a zed camera.
     std::shared_ptr<sl::Camera> p_zed;
     p_zed.reset(new sl::Camera());
 
     STATUS_CODE status_iot;
     status_iot = HubClient::connect("telemetry_app");
-    if (status_iot != STATUS_CODE::SUCCESS) {
+    if (status_iot != STATUS_CODE::SUCCESS)
+    {
         std::cout << "Initiliazation error " << status_iot << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    status_iot = HubClient::registerCamera(p_zed);
-    if (status_iot != STATUS_CODE::SUCCESS) {
-        std::cout << "Camera registration error " << status_iot << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    
-    //Open the ZED camera
+    // Open the ZED camera
     sl::InitParameters initParameters;
     initParameters.camera_resolution = RESOLUTION::HD2K;
 
     sl::ERROR_CODE status_zed = p_zed->open(initParameters);
-    if (status_zed != ERROR_CODE::SUCCESS) {
+    if (status_zed != ERROR_CODE::SUCCESS)
+    {
         HubClient::sendLog("Camera initialization error : " + std::string(toString(status_zed)), LOG_LEVEL::ERROR);
         exit(EXIT_FAILURE);
     }
 
-    //Enable Positional Tracking
+    // Register the camera once it has been open
+    status_iot = HubClient::registerCamera(p_zed);
+    if (status_iot != STATUS_CODE::SUCCESS)
+    {
+        std::cout << "Camera registration error " << status_iot << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Enable Positional Tracking
     PositionalTrackingParameters positional_tracking_param;
     positional_tracking_param.enable_area_memory = true;
     status_zed = p_zed->enablePositionalTracking(positional_tracking_param);
-    if (status_zed != ERROR_CODE::SUCCESS) {
+    if (status_zed != ERROR_CODE::SUCCESS)
+    {
         HubClient::sendLog("Enabling positional tracking failed : " + std::string(toString(status_zed)), LOG_LEVEL::ERROR);
         exit(EXIT_FAILURE);
     }
@@ -74,13 +79,16 @@ int main(int argc, char **argv) {
     runtime_parameters.measure3D_reference_frame = sl::REFERENCE_FRAME::WORLD;
     sl::Timestamp prev_timestamp = 0;
     // Main loop
-    while (true) {
+    while (true)
+    {
         // Grab a new frame from the ZED
         status_zed = p_zed->grab(runtime_parameters);
-        if (status_zed != ERROR_CODE::SUCCESS) break;
+        if (status_zed != ERROR_CODE::SUCCESS)
+            break;
 
         sl::Timestamp curr_timestamp = p_zed->getTimestamp(sl::TIME_REFERENCE::IMAGE);
-        if (curr_timestamp.getMilliseconds() >= prev_timestamp.getMilliseconds() + 1000) {
+        if (curr_timestamp.getMilliseconds() >= prev_timestamp.getMilliseconds() + 1000)
+        {
             // Retrieve camera position
             p_zed->getPosition(cam_pose);
             sl::Translation translation = cam_pose.getTranslation();
@@ -101,23 +109,25 @@ int main(int argc, char **argv) {
         // Insert custom code here
 
         // Always update IoT at the end of the grab loop
-        HubClient::update();
+        HubClient::update(p_zed);
         sleep_ms(1);
     }
 
     p_zed->disablePositionalTracking();
     // Handling camera error
-    if(status_zed != ERROR_CODE::SUCCESS){
-        HubClient::sendLog("Grab failed, restarting camera. "+std::string(toString(status_zed)), LOG_LEVEL::ERROR);
+    if (status_zed != ERROR_CODE::SUCCESS)
+    {
+        HubClient::sendLog("Grab failed, restarting camera. " + std::string(toString(status_zed)), LOG_LEVEL::ERROR);
         p_zed->close();
         sl::ERROR_CODE e = sl::Camera::reboot(p_zed->getCameraInformation().serial_number);
     }
-    //Close the camera
-    else if(p_zed->isOpened())
+    // Close the camera
+    else if (p_zed->isOpened())
         p_zed->close();
-    
+
     status_iot = HubClient::disconnect();
-    if (status_iot != STATUS_CODE::SUCCESS) {
+    if (status_iot != STATUS_CODE::SUCCESS)
+    {
         std::cout << "Terminating error " << status_iot << std::endl;
         exit(EXIT_FAILURE);
     }
