06f1e26382475e5c3c2affab311abb808c99170b
alassagne <antoine.lassagne@stereolabs.com>
2023-01-18 10:01:41 +0100

    update multicam sample cpp

--- tutorials/tutorial_09_multi_cam_stream/cpp/main.cpp
+++ tutorials/tutorial_09_multi_cam_stream/cpp/main.cpp
@@ -25,18 +25,21 @@ using namespace sl;
 using namespace sl_iot;
 using json = sl_iot::json;
 
+#include <chrono>
+#include <thread>
+
 // Secondary streams' loop to grab image
-void secondary_stream_loop(Camera& zed, const std::string& stream_name, bool& run)
+void stream_loop(const std::shared_ptr<Camera> &p_zed, bool &run)
 {
-    std::cout << "Secondary stream (" << stream_name << ") opened" << std::endl;
     Mat zed_image(1280, 720, MAT_TYPE::U8_C4);
 
-    while (run) {
+    while (run)
+    {
         // grab current image
-        if (zed.grab() == ERROR_CODE::SUCCESS)
+        if (p_zed->grab() == ERROR_CODE::SUCCESS)
         {
-            zed.retrieveImage(zed_image, VIEW::LEFT, MEM::CPU, zed_image.getResolution());
-            auto status_code = HubClient::addSecondaryStream(stream_name, zed_image);
+            p_zed->retrieveImage(zed_image, VIEW::LEFT, MEM::CPU, zed_image.getResolution());
+            HubClient::update(p_zed, zed_image);
         }
         else
             run = false;
@@ -55,15 +58,15 @@ int main(int argc, char **argv)
     }
 
     // Get detected cameras
-	std::vector<DeviceProperties> devList = Camera::getDeviceList();
+    std::vector<DeviceProperties> devList = Camera::getDeviceList();
     int nb_detected_zed = devList.size();
-    
+
     if (nb_detected_zed == 0)
     {
         std::cout << "No ZED Detected, exit program" << std::endl;
         exit(EXIT_FAILURE);
     }
-    
+
     std::cout << nb_detected_zed << " ZED detected" << std::endl;
 
     InitParameters initParameters;
@@ -71,63 +74,65 @@ int main(int argc, char **argv)
     initParameters.camera_fps = 30;
     initParameters.depth_mode = DEPTH_MODE::NONE;
 
-    std::vector<Camera> zeds(nb_detected_zed);
+    std::vector<std::shared_ptr<sl::Camera>> zeds(nb_detected_zed);
 
     // Open every detected cameras
-    for (int z = 0; z < nb_detected_zed; z++)
+    for (int i = 0; i < nb_detected_zed; i++)
     {
-        initParameters.input.setFromCameraID(z);
-        ERROR_CODE err = zeds[z].open(initParameters);
+        zeds[i].reset(new sl::Camera());
+        initParameters.input.setFromCameraID(i);
+
+        ERROR_CODE err = zeds[i]->open(initParameters);
         if (err == ERROR_CODE::SUCCESS)
         {
-            auto cam_info = zeds[z].getCameraInformation();
-            std::cout << "serial number: " << cam_info.serial_number << ", model: " <<  cam_info.camera_model << ", status: opened" << std::endl;
+            auto cam_info = zeds[i]->getCameraInformation();
+            std::cout << "serial number: " << cam_info.serial_number << ", model: " << cam_info.camera_model << ", status: opened" << std::endl;
         }
         else
         {
-            std::cout << " Error: " << err << std::endl;
-            zeds[z].close();
+            std::cout << " Error on camera " << i << " : " << err << std::endl;
+            throw(new std::exception());
+        }
+
+        // Register the camera once it has been open
+        UpdateParameters updateParameters;
+
+        // On Ubuntu desktop, on consumer-level GPUs, you don't have enough hardware encoder to stream multiple devices
+        // and to record at the same time. https://en.wikipedia.org/wiki/Nvidia_NVENC
+        // On jetsons or on business-grade gpus, you can do whatever you want.
+        updateParameters.enable_recording = false;
+        status_iot = HubClient::registerCamera(zeds[i], updateParameters);
+        if (status_iot != STATUS_CODE::SUCCESS)
+        {
+            std::cout << "Camera registration error " << status_iot << std::endl;
+            exit(EXIT_FAILURE);
         }
     }
 
-    // Register the first camera as the main one
-    std::shared_ptr<Camera> p_zed;
-    p_zed.reset(&zeds[0]);
-    HubClient::registerCamera(p_zed);
-
     // Thread loops for secondary streams
     bool run_zeds = true;
-    std::vector<thread> thread_pool(nb_detected_zed - 1);
-    for (int z = 1; z < nb_detected_zed; z++)
+    std::vector<thread> thread_pool(nb_detected_zed);
+    for (int i = 0; i < nb_detected_zed; i++)
     {
-        if (zeds[z].isOpened())
+        if (zeds[i]->isOpened())
         {
-            auto cam_info = zeds[z].getCameraInformation();
-            thread_pool[z - 1] = std::thread(secondary_stream_loop, ref(zeds[z]), std::to_string(cam_info.serial_number), ref(run_zeds));
+            thread_pool[i] = std::thread(stream_loop, zeds[i], ref(run_zeds));
         }
     }
 
     // Main loop
     while (run_zeds)
     {
-        // Grab a new frame from the ZED
-        auto status_zed = p_zed->grab();
-
-        if (status_zed != ERROR_CODE::SUCCESS) break;
-
-        // Always update IoT at the end of the grab loop
-        HubClient::update();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
-    
-    run_zeds = false;
-    p_zed->close();
-    
+
     // Wait for every thread to be stopped
-    for (int z = 1; z < nb_detected_zed; z++)
-        if (zeds[z].isOpened()) {
-            thread_pool[z].join();
-            zeds[z].close();
+    for (int i = 0; i < nb_detected_zed; i++)
+        if (zeds[i]->isOpened())
+        {
+            thread_pool[i].join();
+            zeds[i]->close();
         }
-        
+
     return 0;
 }
